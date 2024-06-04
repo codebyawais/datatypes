927a0c38f2717b4dc305800efc5fe9008cf4e08f
WebDevByAwaisRafiq <webdevbyawais@gmail.com>
2024-06-04 20:39:46 +0500

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,24 @@
+// Number  2 to power of 53
+// bigint id data is very large amount
+// String ""
+// Boolean true /false
+// null  standalone value
+// undefined
+// Symbol   unique
+// object
+
+// let name = "awais";
+// console.log(name);
+// let age = 23;
+//   console.log(age);
+  
+// let isloggedin = true
+// console.log(isloggedin);
+
+
+// let Name ;
+// console.log(Name);
+// let Names="" ;
+// console.log(Names);
+
+console.log(typeof  age);
